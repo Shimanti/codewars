ce3f5517dc15958bfe3787652d9fedab4a9d88c6
Shimanti <676501+Shimanti@users.noreply.github.com>
2022-04-22 21:09:40 -0700

    Update practice3.js

--- practice3.js
+++ practice3.js
@@ -66,19 +66,69 @@ console.log(maps([1, 2, 3]))
 // rps('scissors','rock') // Player 2 won!
 // rps('paper','paper') // Draw!
 
-const rps = (p1, p2) => {
-  if (p1 === p2) {
-     return "Draw!"
-  } else if (p1==="scissors" && p2 ==="paper") {
-    return "Player 1 won!"
-  } else if (p1 === "rock" && p2 === "scissors") {
-    return "Player 1 won!"
-  } else if (p1 === "paper" && p2 === "rock") {
-    return "Player 1 won!"
-  } else {
-    return "Player 2 won!"
+//const rps = (p1, p2) => {
+//  if (p1 === p2) {
+//     return "Draw!"
+//  } else if (p1==="scissors" && p2 ==="paper") {
+//    return "Player 1 won!"
+//  } else if (p1 === "rock" && p2 === "scissors") {
+//    return "Player 1 won!"
+// } else if (p1 === "paper" && p2 === "rock") {
+//    return "Player 1 won!"
+// } else {
+//    return "Player 2 won!"
+// }
+// };
+
+// Count the number of Duplicates
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+// Example
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1 # 'i' occurs six times
+// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+
+// function duplicateCount(text){
+//  return (text.toLowerCase().split('').sort().join('').match(/([^])\1+/g) || []).length;
+// }
+
+function duplicateCount(text){
+  
+  var input = text.toLowerCase().split('');
+  
+  var obj = {};
+  
+  for( var i in input) {
+    
+    if(!obj[ input[i] ]){
+      
+      obj[ input[i] ] = 1;
+      
+    } else{
+      obj[ input[i] ] += 1;
+    } 
   }
-   
-};
+  
+  var result = 0;
+  
+  for( var prop in obj) {
+    
+    if(obj[prop] > 1){
+     result++;
+    }
+  }
+  
+  return result;
+  
+}
 
-
+// function duplicateCount(text){
+//  return text.toLowerCase().split('').filter(function(val, i, arr){
+//    return arr.indexOf(val) !== i && arr.lastIndexOf(val) === i;
+//  }).length;
+// }
